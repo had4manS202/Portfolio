02199ea11770dc4be8d185b9d4105982d4ece5ba
had4manS202 <hadamanash2023@gmail.com>
2025-06-06 18:35:21 +0545

    Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-white">Expertise</h2>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-white">Interests</h2>
                 <p className="max-w-[900px] text-slate-400 md:text-xl/relaxed">
                   Specialized in various aspects of cybersecurity and digital protection
                 </p>
